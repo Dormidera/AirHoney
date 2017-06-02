30c4003d9ba3e1b5a7603b041d372014ed5c1696
Dormidera <mdormidera@gmail.com>
2017-06-02 15:01:57 -0400

    Update airhoney.py

--- airhoney.py
+++ airhoney.py
@@ -28,19 +28,23 @@ if opcion == 1:
 	os.system("echo '# Kali linux repositorios \ndeb http://http.kali.org/kali sana main non-free contrib\ndeb http://security.kali.org/kali-security sana/updates main contrib non-free\ndeb http://repo.kali.org/kali kali-bleeding-edge main' >>sudo /etc/apt/sources.list")
 	os.system("apt-key adv --keyserver pgp.mit.edu --recv-keys ED444FF07D8D0BF6")
 	os.system("sudo apt-get update -m")
+	os.system("python airhoney.py")
 	
 elif opcion == 2:
 	print("MacChanger")
 	os.system("sudo apt-get install macchanger macchanger-gtk")
+	os.system("python airhoney.py")
 	
 elif opcion == 3:
 	print("Wifite\nAircrack-ng\nLinset\nBully\nReaver\nPixieWPS\nCowpatty")
 	os.system("sudo apt-get  install libpcap-dev libssl-dev wifite aircrack-ng bully reaver pixiewps cowpatty")
 	os.system("sudo git clone https://github.com/vk496/linset.git")
+	os.system("python airhoney.py")
 
 elif opcion == 4:
 	print("Diccionarios")
 	os.system("sudo git clone https://github.com/Dormidera/Passwords.git")	
+	os.system("python airhoney.py")
 	
 elif opcion == 5:
 	os.system("clear")
